bb6ea10a2be4f4ed7e812413a690529fc21b9cbe
ytchendjou <ytchendjou@prima-solutions.com>
2018-11-04 17:41:09 +0100

    Indemtation

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -18,7 +18,7 @@ angular.module('mscApp')
     	$location.path('/myspace');
         return;
     }
-	$scope.evtName = Session.eventName();
+	  $scope.evtName = Session.eventName();
     $scope.isPlanView = true;
 
     $scope.titles = [
@@ -59,53 +59,53 @@ angular.module('mscApp')
     });
 
     function setMap() {
-		$('#myTabs a').click(function (e) {
-			e.preventDefault();
-			$(this).tab('show');
-			$scope.isPlanView = $('.nav-tabs .active').text() === 'Orga Salle';
-			$scope.$apply();
+      $('#myTabs a').click(function (e) {
+        e.preventDefault();
+        $(this).tab('show');
+        $scope.isPlanView = $('.nav-tabs .active').text() === 'Orga Salle';
+        $scope.$apply();
 
-			// currentTab = e.target.hash;
-		});
+        // currentTab = e.target.hash;
+      });
 
-	    $('a[href="#map"]').tab('show');
+        $('a[href="#map"]').tab('show');
 
-		// trigger the selection on the dropdow "Add table"
-		$('.dropdown-submenu a').on('click', function(e) {
-		    $(this).next('ul').toggle();
-		    e.stopPropagation();
-		    e.preventDefault();
-		});
-	}
-	setMap();
+      // trigger the selection on the dropdow "Add table"
+      $('.dropdown-submenu a').on('click', function(e) {
+          $(this).next('ul').toggle();
+          e.stopPropagation();
+          e.preventDefault();
+      });
+    }
+    setMap();
 
-	/* ******* methods ******** */
+    /* ******* methods ******** */
 
-	/* ******* guest ******** */
+    /* ******* guest ******** */
 
-	// add a new guest
+    // add a new guest
     $scope.addGuest = function() {
 	    var modalInstance = $uibModal.open({
-			templateUrl: '../../views/newGuestPopup.html',
-			controller: 'newGuestPopupCtrl',
-			resolve: {
-				parameters: function () {
-					return {titles: $scope.titles, guestList: $scope.guests, eventId: _evtId};
-				}
-			}
-		});
+        templateUrl: '../../views/newGuestPopup.html',
+        controller: 'newGuestPopupCtrl',
+        resolve: {
+          parameters: function () {
+            return {titles: $scope.titles, guestList: $scope.guests, eventId: _evtId};
+          }
+        }
+      });
 
-		modalInstance.result.then(
-			function (newGuest) { //$uibModalInstance.close
-			    console.log(newGuest);
-			    $scope.guestList.addNodeData(newGuest);
-			    $scope.guestsOrigin.push(newGuest);
-			}, 
-			function (msg) {//$uibModalInstance.dismiss
-				console.log(msg);
-			}
-		);
-	};
+      modalInstance.result.then(
+        function (newGuest) { //$uibModalInstance.close
+            console.log(newGuest);
+            $scope.guestList.addNodeData(newGuest);
+            $scope.guestsOrigin.push(newGuest);
+        },
+        function (msg) {//$uibModalInstance.dismiss
+          console.log(msg);
+        }
+      );
+    };
 
     $scope.deleteGuest = function(guest) {
     	// $scope.guests.splice($scope.guests.indexOf(guest),1);
@@ -131,20 +131,20 @@ angular.module('mscApp')
     	$scope.isSelectedAllGuests = isSelectedAllGuests;
     };
 
-	/* ******* table ******** */
+	  /* ******* table ******** */
 
     var locX = 364.5, locY = 223.5;
-	var tableStd = {'guests':{}};
-	$scope.addTable = function(tableId) {
-    	var previousTables = angular.copy($scope.tables);
-		tableStd.key = $scope.tables.length+1+'';
-		tableStd.category = 'Table'+tableId;
-		tableStd.name = tableStd.key + '';
-		tableStd.loc = (locX + 2*tableStd.key) + ' ' + (locY + 2*tableStd.key);
-		// tableStd.loc = previousTables[previousTables.length-1].loc;
-		previousTables.push(angular.copy(tableStd));
-		$scope.tables = previousTables;
-	};
+    var tableStd = {'guests':{}};
+    $scope.addTable = function(tableId) {
+        var previousTables = angular.copy($scope.tables);
+      tableStd.key = $scope.tables.length+1+'';
+      tableStd.category = 'Table'+tableId;
+      tableStd.name = tableStd.key + '';
+      tableStd.loc = (locX + 2*tableStd.key) + ' ' + (locY + 2*tableStd.key);
+      // tableStd.loc = previousTables[previousTables.length-1].loc;
+      previousTables.push(angular.copy(tableStd));
+      $scope.tables = previousTables;
+    };
 
     var triggerTime = 0;
     $scope.triggerPosition = function() {
@@ -152,17 +152,17 @@ angular.module('mscApp')
     	var data = model.findNodeDataForKey($scope.guestsOrigin[0].key);
     	if(!data) {
     		model = $scope.guestList;
-			data = model.findNodeDataForKey($scope.guests[0].key);
-			if(!data) {
-				return;
-			}
+        data = model.findNodeDataForKey($scope.guests[0].key);
+        if(!data) {
+          return;
+        }
     	}
 
     	triggerTime++;
-		if(triggerTime === 10) {
-			triggerTime = 0;
-			return;
-		}
+      if(triggerTime === 10) {
+        triggerTime = 0;
+        return;
+      }
 
     	setTimeout(function(){ model.setDataProperty(data, 'fill', 'green'); }, 100);
     	setTimeout(function(){ model.setDataProperty(data, 'fill', 'blanchedalmond'); }, 200);
@@ -177,80 +177,80 @@ angular.module('mscApp')
         });
     }
 
-	function saveTable(table, idx) {
-		if(table._id) {
-			ServiceAjax.tables().set(table).then(function(data) {
-				console.log(data);
-	        }, function(data) {
-	            console.log('Error: ' + data);
-	        });
-		} else {
-			ServiceAjax.tables().create(table).then(function(data) {
-				console.log(data);
-				$scope.tables[idx]._id = data.data._id;
-	        }, function(data) {
-	            console.log('Error: ' + data);
-	        });
-		}
-	}   
+    function saveTable(table, idx) {
+      if(table._id) {
+        ServiceAjax.tables().set(table).then(function(data) {
+          console.log(data);
+            }, function(data) {
+                console.log('Error: ' + data);
+            });
+      } else {
+        ServiceAjax.tables().create(table).then(function(data) {
+          console.log(data);
+          $scope.tables[idx]._id = data.data._id;
+          }, function(data) {
+              console.log('Error: ' + data);
+          });
+      }
+    }
 
     $scope.saveMap = function() {
     	console.log('save me');
-		$scope.tables.forEach(function(item, idx, tables) {
-			if(item.hasOwnProperty('guests')) {
-				saveTable(item, idx);
-			} else {
-				saveGuest(item);
-			}
+      $scope.tables.forEach(function(item, idx, tables) {
+        if(item.hasOwnProperty('guests')) {
+          saveTable(item, idx);
+        } else {
+          saveGuest(item);
+        }
 
-			if(idx === tables.length-1) {
-				console.log('saved');
-			}
-		});
+        if(idx === tables.length-1) {
+          console.log('saved');
+        }
+      });
     };
 
-	$scope.isFullScreen = false;
-	$scope.goFullScreen = function(elt) {
-		$scope.isFullScreen = true;
-		elt = elt || document.getElementById('myFlexDiv');
-	    if (elt.mozRequestFullScreen) {
-			elt.mozRequestFullScreen();
-	    } else if (elt.webkitRequestFullScreen) {
-			elt.webkitRequestFullScreen();
-   		}
-   		$scope.displayMode = 'DMBack';
-		$scope.isFullScreen = false;
-	};
+    $scope.isFullScreen = false;
+    $scope.goFullScreen = function(elt) {
+      $scope.isFullScreen = true;
+      elt = elt || document.getElementById('myFlexDiv');
+        if (elt.mozRequestFullScreen) {
+        elt.mozRequestFullScreen();
+        } else if (elt.webkitRequestFullScreen) {
+        elt.webkitRequestFullScreen();
+        }
+        $scope.displayMode = 'DMBack';
+      $scope.isFullScreen = false;
+    };
 
-	/* ******* watches ******** */
+    /* ******* watches ******** */
 
-	$scope.$watch('map.guests', function(new_, old_) {
-		if (new_ !== old_) {
-			$scope.guestList = new go.GraphLinksModel(new_);
-		}
-	});
+    $scope.$watch('map.guests', function(new_, old_) {
+      if (new_ !== old_) {
+        $scope.guestList = new go.GraphLinksModel(new_);
+      }
+    });
 
-	$scope.$watch('map.tables', function(new_, old_) {
-		if (new_ !== old_) {
-			$scope.model = new go.GraphLinksModel(new_);
-		}
-	});
+    $scope.$watch('map.tables', function(new_, old_) {
+      if (new_ !== old_) {
+        $scope.model = new go.GraphLinksModel(new_);
+      }
+    });
 
-    /* ******* godiagram.js  ********* */
+      /* ******* godiagram.js  ********* */
 
-	$scope.guestList = new go.GraphLinksModel($scope.guests);
+    $scope.guestList = new go.GraphLinksModel($scope.guests);
 
-	$scope.model = new go.GraphLinksModel($scope.tables);
-	$scope.model.selectedNodeData = null;
-	$scope.replaceModel = function() {
-		$scope.model = new go.GraphLinksModel(
-			[
-				{ key: 11, name: 'zeta', color: 'red' },
-				{ key: 12, name: 'eta', color: 'green' }
-			],
-			[
-				{ from: 11, to: 12 }
-			]
-		);
-	};  
+    $scope.model = new go.GraphLinksModel($scope.tables);
+    $scope.model.selectedNodeData = null;
+    $scope.replaceModel = function() {
+      $scope.model = new go.GraphLinksModel(
+        [
+          { key: 11, name: 'zeta', color: 'red' },
+          { key: 12, name: 'eta', color: 'green' }
+        ],
+        [
+          { from: 11, to: 12 }
+        ]
+      );
+    };
 });
