6e1e017cee47c1dfd478ac7cda393f6409a03606
ytchendjou <ytchendjou@prima-solutions.com>
2018-11-23 19:59:59 +0100

    Home page

--- app/scripts/controllers/myspace.js
+++ app/scripts/controllers/myspace.js
@@ -131,6 +131,46 @@ angular.module('mscApp')
       $scope.$apply();
     });
 
+    function map() {
+      var locations = [
+          ['Club PAD de Bonanjo',  4.029837 , 9.687983,1],
+          ['Cathédrale Saint Pierre et Paul', 4.044632, 9.692685,2],
+          ['Espace Perenco(En face Palais de justice)', 4.043685, 9.687057,3],
+      ];
+
+      var map = new google.maps.Map(document.getElementById('map500'), {
+        center: new google.maps.LatLng( 4.074742, 9.673119),
+        zoom: 12,
+        scrollwheel: false,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+
+      });
+
+      var infowindow = new google.maps.InfoWindow();
+
+      var marker, i;
+
+      for (i = 0; i < locations.length; i++) {
+        marker = new google.maps.Marker({
+          position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+          map: map,
+          icon:'../../images/map-marker.png'
+        });
+
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+          return function() {
+            infowindow.setContent(locations[i][0]);
+            infowindow.open(map, marker);
+          }
+        })(marker, i));
+      }
+    };
+    $scope.$on('$viewContentLoaded', function(){
+      if ($(".map").length) {
+        map();
+      }
+    });
+
     /*****************/
 
     // configuration datePicker
